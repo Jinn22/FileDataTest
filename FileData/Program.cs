c0914c3eff8131639a24e8e13a06bcae4e9a1bf5
jin <kongxiujin@gmail.com>
2018-04-18 10:06:09 +0100

    Initial commit

---
+++ FileData/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        static FileDetails fd = new FileDetails();
+        /* Expected arguments: 
+         *  First argument: indicate the functionality to perform
+         *  Second argument: indicate the filename
+         */
+        public static void Main(string[] args)
+        {
+            String result = chooseFunctionality(args);
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+
+        /*This function takes the args parameters and based on the 
+         functionality return version or size of the file*/
+        public static string chooseFunctionality(String [] args) {
+            try
+            {
+                switch (args[0])
+                {
+                    case "-v":
+                    case "--v":
+                    case "/v":
+                    case "--version":
+                        return fd.Version(args[1]);
+                    case "-s":
+                    case "--s":
+                    case "/s":
+                    case "--size":
+                        return fd.Size(args[1]).ToString();
+                    default:
+                        return "Wrong input";
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                return "Wrong input";
+            }
+        }
+    }
+}
---
+++ FileDataTests/Tests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+using System;
+using ThirdPartyTools;
+
+namespace FileData.Tests
+{
+    [TestClass()]
+    public class Tests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestNoArgs()
+        {
+            String actual = Program.chooseFunctionality(null);
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsVersion()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = {"-v", "c:test.txt" };
+            String actual = Program.chooseFunctionality(args);
+            Regex regex = new Regex(@"\d*\.\d*\.\d*");
+            Match match = regex.Match(actual);
+            if (!match.Success)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsSize()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "-s", "c:test.txt" };
+            int actual = Int32.Parse(Program.chooseFunctionality(args));
+            if (actual < 0 && actual > 200000)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestOneArgs()
+        {   FileDetails fd = new FileDetails();
+            String[] args = { "-v" };
+            String actual = Program.chooseFunctionality(args);
+            Assert.AreEqual(actual, "Wrong input");
+        }
+
+        [TestMethod()]
+        public void TestWrongInput()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "wrong functionality", "wrong file path" };
+            String actual = Program.chooseFunctionality(args);
+            Assert.AreEqual(actual, "Wrong input");
+        }
+    }
+}
