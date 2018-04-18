65e7042d52e4b4ea755b1f492b845daf9202786b
jin <kongxiujin@gmail.com>
2018-04-18 10:18:33 +0100

    Added more test methods

--- FileDataTests/Tests.cs
+++ FileDataTests/Tests.cs
@@ -16,28 +16,6 @@ namespace FileData.Tests
         }
 
         [TestMethod()]
-        public void TestCorrectArgsVersion()
-        {
-            FileDetails fd = new FileDetails();
-            String[] args = {"-v", "c:test.txt" };
-            String actual = Program.chooseFunctionality(args);
-            Regex regex = new Regex(@"\d*\.\d*\.\d*");
-            Match match = regex.Match(actual);
-            if (!match.Success)
-                Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void TestCorrectArgsSize()
-        {
-            FileDetails fd = new FileDetails();
-            String[] args = { "-s", "c:test.txt" };
-            int actual = Int32.Parse(Program.chooseFunctionality(args));
-            if (actual < 0 && actual > 200000)
-                Assert.Fail();
-        }
-
-        [TestMethod()]
         public void TestOneArgs()
         {   FileDetails fd = new FileDetails();
             String[] args = { "-v" };
@@ -52,6 +30,96 @@ namespace FileData.Tests
             String[] args = { "wrong functionality", "wrong file path" };
             String actual = Program.chooseFunctionality(args);
             Assert.AreEqual(actual, "Wrong input");
+        }
+
+        /*Correct version functionality parameter: "-v", "--v", "/v", "--version"*/
+        [TestMethod()]
+        public void TestCorrectArgsVersion1()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "-v", "c:test.txt" };
+            String actual = Program.chooseFunctionality(args);
+            Regex regex = new Regex(@"\d*\.\d*\.\d*");
+            Match match = regex.Match(actual);
+            if (!match.Success)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsVersion2()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "--v", "c:test.txt" };
+            String actual = Program.chooseFunctionality(args);
+            Regex regex = new Regex(@"\d*\.\d*\.\d*");
+            Match match = regex.Match(actual);
+            if (!match.Success)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsVersion3()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "/v", "c:test.txt" };
+            String actual = Program.chooseFunctionality(args);
+            Regex regex = new Regex(@"\d*\.\d*\.\d*");
+            Match match = regex.Match(actual);
+            if (!match.Success)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsVersion4()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "--version", "c:test.txt" };
+            String actual = Program.chooseFunctionality(args);
+            Regex regex = new Regex(@"\d*\.\d*\.\d*");
+            Match match = regex.Match(actual);
+            if (!match.Success)
+                Assert.Fail();
+        }
+
+        /*Correct size functionality parameter: "-s", "--s", "/s", "--size"*/
+        [TestMethod()]
+        public void TestCorrectArgsSize1()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "-s", "c:test.txt" };
+            int actual = Int32.Parse(Program.chooseFunctionality(args));
+            if (actual < 0 && actual > 200000)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsSize2()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "--s", "c:test.txt" };
+            int actual = Int32.Parse(Program.chooseFunctionality(args));
+            if (actual < 0 && actual > 200000)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsSize3()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "/s", "c:test.txt" };
+            int actual = Int32.Parse(Program.chooseFunctionality(args));
+            if (actual < 0 && actual > 200000)
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TestCorrectArgsSize4()
+        {
+            FileDetails fd = new FileDetails();
+            String[] args = { "--size", "c:test.txt" };
+            int actual = Int32.Parse(Program.chooseFunctionality(args));
+            if (actual < 0 && actual > 200000)
+                Assert.Fail();
         }
     }
 }
